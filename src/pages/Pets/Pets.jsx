e7eca188a322fc93119753e3d4eece89fbbc7613
lauraxavier <laura.ac.xavier@gmail.com>
2023-04-28 21:52:40 -0300

    [FE-2] Integrar a remoção de Pets#3

--- src/pages/Pets/Pets.jsx
+++ src/pages/Pets/Pets.jsx
@@ -1,14 +1,26 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Button, Card } from "react-bootstrap";
+import { Button, Card, Modal } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import { Loader } from "../../components/Loader/Loader";
 import avatar from "../../assets/vet.png";
+import { toast } from "react-hot-toast";
 import "./style.css";
 
 export function Pets() {
   const [pets, setPets] = useState(null);
+  const [show, setShow] = useState(false);
+  const [idPet, setIdPet] = useState(null);
 
+ 
+  const handleClose = () => {
+    setIdPet(null);
+    setShow(false);
+  };
+  const handleShow = (id) => {
+    setIdPet(id);
+    setShow(true);
+  };
   useEffect(() => {
     initializeTable();
   }, []);
@@ -24,6 +36,25 @@ export function Pets() {
       });
   }
   const navigate = useNavigate();
+   function onDelete() {
+     axios
+       .delete(`http://localhost:3001/pets/${idPet}`)
+       .then((response) => {
+         toast.success(response.data.message, {
+           position: "bottom-right",
+           duration: 2000,
+         });
+         initializeTable();
+       })
+       .catch((error) => {
+         console.log(error);
+         toast.error(error.response.data.message, {
+           position: "bottom-right",
+           duration: 2000,
+         });
+       });
+     handleClose();
+   }
 
   return (
     <div className="container container-lista-pets">
@@ -39,43 +70,64 @@ export function Pets() {
         ) : (
           pets.map((pet) => {
             return (
-              <Card key={pet.id} className="card-pet">
-                <div
-                  onClick={() => navigate(`/pets/pet/${pet.id}`)}
-                  style={{ cursor: "pointer" }}
-                >
-                  <div className="card-img">
-                    <img src={avatar} alt="" />
+              <>
+                <Card key={pet.id} className="card-pet">
+                  <div
+                    onClick={() => navigate(`/pets/pet/${pet.id}`)}
+                    style={{ cursor: "pointer" }}
+                  >
+                    <div className="card-img">
+                      <img src={avatar} alt="" />
+                    </div>
+                    <Card.Body className="card-body">
+                      <span>Nome:</span>
+                      <Card.Title>
+                        {pet.nome.charAt(0).toUpperCase() + pet.nome.slice(1)}
+                      </Card.Title>
+                      <span>Raça:</span>
+                      <Card.Title>
+                        {pet.tipo.split(" ")[0].charAt(0).toUpperCase() +
+                          pet.tipo.split(" ")[0].slice(1)}
+                      </Card.Title>
+                      <span>Porte:</span>
+                      <Card.Title>
+                        {pet.porte.charAt(0).toUpperCase() + pet.porte.slice(1)}
+                      </Card.Title>
+                      <span>Nascimento:</span>
+                      <Card.Title>
+                        {new Date(
+                          `${pet.dataNasc}T00:00:00`
+                        ).toLocaleDateString("pt-BR")}
+                      </Card.Title>
+                    </Card.Body>
                   </div>
-                  <Card.Body className="card-body">
-                    <span>Nome:</span>
-                    <Card.Title>
-                      {pet.nome.charAt(0).toUpperCase() + pet.nome.slice(1)}
-                    </Card.Title>
-                    <span>Raça:</span>
-                    <Card.Title>
-                      {pet.tipo.split(" ")[0].charAt(0).toUpperCase() +
-                        pet.tipo.split(" ")[0].slice(1)}
-                    </Card.Title>
-                    <span>Porte:</span>
-                    <Card.Title>
-                      {pet.porte.charAt(0).toUpperCase() + pet.porte.slice(1)}
-                    </Card.Title>
-                    <span>Nascimento:</span>
-                    <Card.Title>
-                      {new Date(`${pet.dataNasc}T00:00:00`).toLocaleDateString( "pt-BR")}
-                    </Card.Title>
-                  </Card.Body>
-                </div>
-                <div className="container-btn-pets">
-                  <Button to={"/"}>
-                    <i className="bi bi-trash-fill"></i>
-                  </Button>
-                  <Button>
-                    <i className="bi bi-pencil-fill"></i>
-                  </Button>
-                </div>
-              </Card>
+                  <div className="container-btn-pets">
+                    <Button onClick={() => handleShow(pet.id)}>
+                      <i className="bi bi-trash-fill"></i>
+                    </Button>
+                    <Button to={`/`}>
+                      <i className="bi bi-pencil-fill"></i>
+                    </Button>
+                  </div>
+                </Card>
+                <Modal show={show} onHide={handleClose}>
+                  <Modal.Header closeButton>
+                    <Modal.Title>Confirmação</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body>
+                    Tem certeza que deseja excluir o pet{" "}
+                    {idPet && pets.find((pet) => pet.id === idPet)?.nome} ?
+                  </Modal.Body>
+                  <Modal.Footer>
+                    <Button variant="danger" onClick={handleClose}>
+                      Cancelar
+                    </Button>
+                    <Button variant="primary" onClick={onDelete}>
+                      Excluir
+                    </Button>
+                  </Modal.Footer>
+                </Modal>
+              </>
             );
           })
         )}
