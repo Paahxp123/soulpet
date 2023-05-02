1f9d3e7292e1cc37ec1a13e4bb12e9f2c3f2e489
Paahxp123 <joaopedro2k2294@gmail.com>
2023-05-02 10:20:30 -0300

    Criação da pagina de serviços, criação das funções adicionar, editar e deletar serviços

--- src/App.js
+++ src/App.js
@@ -8,7 +8,14 @@ import { NovoPet } from "./pages/NovoPet/NovoPet";
 import { Pets } from "./pages/Pets/Pets";
 import { PetSingle } from "./pages/PetSingle/PetSingle";
 import { AtualizarProduto } from "./pages/AtualizarProduto/AtualizarProduto";
+<<<<<<< Updated upstream
 import { EditaPet } from "./pages/EditaPet/EditaPet";
+=======
+import { Servicos } from "./pages/Servicos/Servicos";
+import { NovoServico } from "./pages/Servicos/NovoServico";
+import { EditaServicos } from "./pages/Servicos/EditarServico";
+
+>>>>>>> Stashed changes
 
 function App() {
   return (
@@ -24,6 +31,9 @@ function App() {
           <Route path="/clientes/novo" element={<NovoCliente />} />
           <Route path="/clientes/editar/:id" element={<EditaCliente />} />
           <Route path="/produtos/atualizar/:id" element={<AtualizarProduto />} />
+          <Route path="/servicos" element={<Servicos />} />
+          <Route path="/servicos/novo" element={<NovoServico />} />
+          <Route path="/servicos/editar/:id" element={<EditaServicos />} />
         </Route>
       </Routes>
     </BrowserRouter>
