b8ab5646ddd0aaf28009d45e7eea4ca8ddd05955
lauraxavier <laura.ac.xavier@gmail.com>
2023-05-03 22:33:09 -0300

    Formulário de Produtos

--- src/pages/InsercaoDeProdutos/InsercaoDeProdutos.jsx
+++ src/pages/InsercaoDeProdutos/InsercaoDeProdutos.jsx
@@ -1,79 +1,190 @@
-import React from 'react';
+import React, {useState} from "react";
 import axios from "axios";
 import { Button, Form } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
+import avatar from "../../assets/icons/produtoAvatar.png";
 
 export function InsercaoDeProdutos() {
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    const navigate = useNavigate();
+  const [nome, setNome] = useState("");
+  
+  function handleNomeChange(event) {
+    setNome(event.target.value);
+  }
 
-    function onSubmit(data) {
-        axios.post("http://localhost:3001/Produtos", data)
-            .then(response => {
-                toast.success("Produto adicionado.", { position: "bottom-right", duration: 2000 });
-                navigate("/produto");
-            })
-            .catch(error => {
-                toast.error("Algo deu errado.", { position: "bottom-right", duration: 2000 });
-                console.log(error);
-            });
-    }
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const navigate = useNavigate();
 
-    return (
-        <div className="container">
-            <h1>Novo Produto</h1>
+  function onSubmit(data) {
+    axios
+      .post("http://localhost:3001/Produtos", data)
+      .then((response) => {
+        toast.success("Produto adicionado.", {
+          position: "bottom-right",
+          duration: 2000,
+        });
+        navigate("/produto");
+      })
+      .catch((error) => {
+        toast.error("Algo deu errado.", {
+          position: "bottom-right",
+          duration: 2000,
+        });
+        console.log(error);
+      });
+  }
+
+  return (
+    <div className="form mt-5">
+      <div className="container">
+        <Button as={Link} to="/produto" className="btn-brown">
+          <i className="bi bi-arrow-bar-left"></i> Voltar
+        </Button>
+        <div className="container-form">
+          <div className="d-flex flex-column align-items-center gap-3">
+            <div className="avatar-pet">
+              <img src={avatar} alt="SoulPet" />
+            </div>
+            <h3>{nome ? nome : "Produto"}</h3>
+          </div>
+          <div className="form-content">
             <Form onSubmit={handleSubmit(onSubmit)}>
-                <Form.Group className="mb-3">
-                    <Form.Label>Produto:</Form.Label>
-                    <Form.Control type="text" className={errors.nome && "is-invalid"} {...register("nome", { required: "O nome do produto é obrigatório.", maxLength: { value: 130, message: "Limite de 130 caracteres." } })} />
-                    {errors.nome && <Form.Text className="invalid-feedback">{errors.nome.message}</Form.Text>}
-                </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Produto:</Form.Label>
+                <Form.Control
+                  type="text"
+                  className={errors.nome && "is-invalid"}
+                  {...register("nome", {
+                    required: "O nome do produto é obrigatório.",
+                    maxLength: {
+                      value: 130,
+                      message: "Limite de 130 caracteres.",
+                    },
+                  })}
+                  onChange={handleNomeChange}
+                />
+                {errors.nome && (
+                  <Form.Text className="invalid-feedback">
+                    {errors.nome.message}
+                  </Form.Text>
+                )}
+              </Form.Group>
 
-                <Form.Group className="mb-3">
-                    <Form.Label>Preço do Produto:</Form.Label>
-                    <Form.Control type="number" className={errors.preco && "is-invalid"} {...register("preco", { required: "O preço do produto é obrigatório.", min: { value: 0, message: "O preço deve ser maior ou igual a zero." } })} />
-                    {errors.preco && <Form.Text className="invalid-feedback">{errors.preco.message}</Form.Text>}
-                </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Preço do Produto:</Form.Label>
+                <Form.Control
+                  type="number"
+                  className={errors.preco && "is-invalid"}
+                  {...register("preco", {
+                    required: "O preço do produto é obrigatório.",
+                    min: {
+                      value: 0,
+                      message: "O preço deve ser maior ou igual a zero.",
+                    },
+                  })}
+                />
+                {errors.preco && (
+                  <Form.Text className="invalid-feedback">
+                    {errors.preco.message}
+                  </Form.Text>
+                )}
+              </Form.Group>
 
-                <Form.Group className="mb-3">
-                    <Form.Label>Desconto:</Form.Label>
-                    <Form.Control type="number" className={errors.desconto && "is-invalid"} {...register("desconto", { required: "O desconto é obrigatório.", min: { value: 0, message: "O desconto deve ser maior ou igual a zero." }, max: { value: 100, message: "O desconto não pode ser maior que 100." } })} />
-                    {errors.desconto && <Form.Text className="invalid-feedback">{errors.desconto.message}</Form.Text>}
-                </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Desconto:</Form.Label>
+                <Form.Control
+                  type="number"
+                  className={errors.desconto && "is-invalid"}
+                  {...register("desconto", {
+                    required: "O desconto é obrigatório.",
+                    min: {
+                      value: 0,
+                      message: "O desconto deve ser maior ou igual a zero.",
+                    },
+                    max: {
+                      value: 100,
+                      message: "O desconto não pode ser maior que 100.",
+                    },
+                  })}
+                />
+                {errors.desconto && (
+                  <Form.Text className="invalid-feedback">
+                    {errors.desconto.message}
+                  </Form.Text>
+                )}
+              </Form.Group>
 
-                <Form.Group className="mb-3">
-                    <Form.Label>Data do Desconto:</Form.Label>
-                    <Form.Control type="date" className={errors.dataDesconto && "is-invalid"} {...register("dataDesconto", { required: "A data de expiração do desconto é obrigatória." })} />
-                    {errors.dataDesconto && <Form.Text className="invalid-feedback">{errors.dataDesconto.message}</Form.Text>}
-                </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Data do Desconto:</Form.Label>
+                <Form.Control
+                  type="date"
+                  className={errors.dataDesconto && "is-invalid"}
+                  {...register("dataDesconto", {
+                    required: "A data de expiração do desconto é obrigatória.",
+                  })}
+                />
+                {errors.dataDesconto && (
+                  <Form.Text className="invalid-feedback">
+                    {errors.dataDesconto.message}
+                  </Form.Text>
+                )}
+              </Form.Group>
 
-                <Form.Group className="mb-3">
-                    <Form.Label>Categoria do Produto:</Form.Label>
-                    <Form.Control as="select" className={errors.categoria && "is-invalid"} {...register("categoria", { required: "A categoria do produto é obrigatória." })}>
-                        <option value="">Selecione uma opção</option>
-                        <option value="Higiene">Higiêne</option>
-                        <option value="Brinquedos">Brinquedos</option>
-                        <option value="Marcas">Marcas</option>
-                        <option value="Cachorro">Cachorro</option>
-                        <option value="Gato">Gato</option>
-                    </Form.Control>
-                    {errors.categoria && <Form.Text className="invalid-feedback">{errors.categoria.message}</Form.Text>}
-                </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Categoria do Produto:</Form.Label>
+                <Form.Control
+                  as="select"
+                  className={errors.categoria && "is-invalid"}
+                  {...register("categoria", {
+                    required: "A categoria do produto é obrigatória.",
+                  })}
+                >
+                  <option value="">Selecione uma opção</option>
+                  <option value="Higiene">Higiêne</option>
+                  <option value="Brinquedos">Brinquedos</option>
+                  <option value="Marcas">Marcas</option>
+                  <option value="Cachorro">Cachorro</option>
+                  <option value="Gato">Gato</option>
+                </Form.Control>
+                {errors.categoria && (
+                  <Form.Text className="invalid-feedback">
+                    {errors.categoria.message}
+                  </Form.Text>
+                )}
+              </Form.Group>
 
-                <Form.Group className="mb-3">
-                    <Form.Label>Descrição do Produto:</Form.Label>
-                    <Form.Control as="textarea" className={errors.descricao && "is-invalid"} {...register("descricao", { required: "A descrição do produto é obrigatória.", maxLength: { value: 500, message: "Limite de 500 caracteres." } })} />
-                    {errors.descricao && <Form.Text className="invalid-feedback">{errors.descricao.message}</Form.Text>}
-                </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Descrição do Produto:</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  className={errors.descricao && "is-invalid"}
+                  {...register("descricao", {
+                    required: "A descrição do produto é obrigatória.",
+                    maxLength: {
+                      value: 500,
+                      message: "Limite de 500 caracteres.",
+                    },
+                  })}
+                />
+                {errors.descricao && (
+                  <Form.Text className="invalid-feedback">
+                    {errors.descricao.message}
+                  </Form.Text>
+                )}
+              </Form.Group>
 
-                <Button variant="primary" type="submit">
-                    Cadastrar
-                </Button>
+              <Button className="btn-formPet" type="submit">
+                Cadastrar
+              </Button>
             </Form>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
-
-
